c717a293b629380fb39dfb1f5866885c75d5d787
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-02-17 23:40:14 +0100

    Dirty fix for overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:tuple/tuple.dart';
-import 'package:window_size/window_size.dart';
 
 import 'src/app.dart';
 import 'src/mixins/app_version.dart';
@@ -57,8 +56,8 @@ Future<List<OperatingSystem>> loadOperatingSystems(bool showUbuntus) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Don't forget to also change the size in linux/my_application.cc:50
-  setWindowMinSize(const Size(692, 580));
-  setWindowMaxSize(const Size(692, 580));
+  // setWindowMinSize(const Size(692, 580));
+  // setWindowMaxSize(const Size(692, 580));
   final foundQuickGet = await Process.run('which', ['quickget']);
   if (foundQuickGet.exitCode == 0) {
     gOperatingSystems = await loadOperatingSystems(false);
--- lib/src/widgets/home_page/logo.dart
+++ lib/src/widgets/home_page/logo.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Logo extends StatelessWidget {
-  const Logo({
-    Key? key,
-  }) : super(key: key);
+  const Logo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 250,
-      child: Flex(
-        direction: Axis.vertical,
-        children: [
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset('assets/images/logo_pink.png'),
-              ),
-            ),
-          ),
-        ],
-      ),
+    return Expanded(
+      child: Image.asset('assets/images/logo_pink.png'),
     );
   }
 }
